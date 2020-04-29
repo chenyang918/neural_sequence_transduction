fix likelihood calculation

model/CTCNetwork.py
@@ -29,17 +29,19 @@ class ConnectionistTemporalClassification(nn.Module):
         lstm_feats, _ = pad_packed_sequence(output, batch_first=True)  # h dim = B x t_k x n
         lstm_feats = lstm_feats.contiguous()
         logits = self.clf(lstm_feats)
+        logprobs = logits.log_softmax(dim=2)
+        return logprobs
 
-        return logits
-
-    def get_loss(self, logits, phone, length, **kwargs):
-        b, t_k, d = list(logits.size())
+    def get_loss(self, logprobs, phone, length):
+        b, t_k, d = list(logprobs.size())
         loss = 0
         for i in range(b):
-            curr_len = length[i]#.cpu().data.numpy()
-            curr_logit = logits[i]#.cpu().data.numpy()
-            curr_phone = phone[i]#.cpu().data.numpy()
-            loss += self.get_ctc_loss_single(curr_logit, curr_len, curr_phone)
+            curr_len = length[i]
+            curr_logit_logprob = logprobs[i]
+            curr_phone = phone[i]
+            neg_log_likelyhood_single = -self.ctc_likelihood_single(curr_logit_logprob,
+                                                                       curr_len, curr_phone)
+            loss += neg_log_likelyhood_single
         return loss
 
     '''
@@ -56,39 +58,42 @@ class ConnectionistTemporalClassification(nn.Module):
         idx = s // 2
         return idx > 1 and phone[s // 2 - 1] == phone[s // 2 - 2]
 
-    def get_ctc_loss_single(self, y, T, phone):
+    def ctc_likelihood_single(self, log_y, T, phone):
         num_phone = len(phone)
         S = 2*num_phone + 1
-        alpha = torch.zeros((T + 1, S + 1))
+        log_alpha = torch.zeros((T + 1, S + 1))
         t, s = 1, 1
-        alpha[t, s] = y[t-1, self.get_phone_id(s, phone)]
+        log_alpha[t, s] = log_y[t-1, self.get_phone_id(s, phone)]
         t, s = 1, 2
-        alpha[t, s] = y[t - 1, self.get_phone_id(s, phone)]
+        log_alpha[t, s] = log_y[t - 1, self.get_phone_id(s, phone)]
         for t in range(2, T + 1):
             for s in range(1, S + 1):
-                y_ = y[t - 1, self.get_phone_id(s, phone)]
+                y_ = log_y[t - 1, self.get_phone_id(s, phone)]
                 # blank or same labels
                 if s % 2 == 1 or self.has_same_label(s, phone):
-                    alpha[t, s] = (alpha[t-1, s] + alpha[t-1, s-1]) * y_
+                    sum_prev = torch.stack([log_alpha[t-1, s], log_alpha[t - 1, s-1]])
+                    log_alpha[t, s] = sum_prev.logsumexp(dim=0) + y_
                 else:
-                    alpha[t, s] = (alpha[t-1, s] + alpha[t-1, s-1] + alpha[t-1, s-2]) * y_
-        return alpha[T, S] + alpha[T, S - 1]
+                    sum_prev = torch.stack([log_alpha[t - 1, s],
+                                            log_alpha[t - 1, s - 1], log_alpha[t - 1, s-2]])
+                    log_alpha[t, s] = sum_prev.logsumexp(dim=0) + y_
+        log_likelihood = torch.stack([log_alpha[T, S], log_alpha[T, S - 1]]).logsumexp(dim=0)
+        return log_likelihood
 
     def path_to_str(self, path):
         new_path = [path[0]] + [path[i] for i in range(1, len(path)) if path[i-1] != path[i]]
         return [p -1 for p in new_path if p > 0]
 
-    def best_path_decode(self, logits, length):
-        logits = logits.cpu().data.numpy()
+    def best_path_decode(self, logprobs, length):
+        logprobs = logprobs.cpu().data.numpy()
         length = length.cpu().data.numpy()
         out_paths = []
-        for y, curr_len in zip(logits, length):
+        for y, curr_len in zip(logprobs, length):
             path = np.argmax(y[:curr_len, :], axis=1)
             out_paths.append(self.path_to_str(path))
         return out_paths
 
-    def prefix_search_decode(self, logits, length):
-        logprobs = logits.log_softmax(dim=2)
+    def prefix_search_decode(self, logprobs, length):
         logprobs = logprobs.cpu().data.numpy()
         length = length.cpu().data.numpy()
 

model/beam_decoder.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-def exp_sum(a, b):
-    return np.log(np.exp(a) + np.exp(b))
+def logsumexp(a, b):
+    m = np.max(a,b)
+    return np.log(np.exp(a-m) + np.exp(b-m)) + m
+
 class GammaEntry(object):
     def __init__(self):
         self.blank = None
@@ -69,14 +71,14 @@ def prefix_beam_search(logprob, T, num_tags):
             for t in range(1, T):
                 new_label_prob = p_star.gamma[t - 1].blank
                 if len(p_star.path) == 0 or p_star.path[-1] != k:
-                    new_label_prob = exp_sum(new_label_prob, p_star.gamma[t - 1].other)
+                    new_label_prob = logsumexp(new_label_prob, p_star.gamma[t - 1].other)
                 gamma_entry = GammaEntry()
-                gamma_entry.other = logprob[t, k] + exp_sum(new_label_prob, gamma[t - 1].other)
-                gamma_entry.blank = logprob[t, 0] + exp_sum(gamma[t - 1].blank, gamma[t - 1].other)
+                gamma_entry.other = logprob[t, k] + logsumexp(new_label_prob, gamma[t - 1].other)
+                gamma_entry.blank = logprob[t, 0] + logsumexp(gamma[t - 1].blank, gamma[t - 1].other)
                 gamma.append(gamma_entry)
-                prefix_prob = exp_sum(prefix_prob, (logprob[t, k] + new_label_prob))
+                prefix_prob = logsumexp(prefix_prob, (logprob[t, k] + new_label_prob))
 
-            P_path_full = exp_sum(gamma[T - 1].blank, gamma[T - 1].other)
+            P_path_full = logsumexp(gamma[T - 1].blank, gamma[T - 1].other)
             P_path_partial = prefix_prob - P_path_full
 
             prob_remaining = prob_remaining - P_path_partial